8f9adda4094af800d0470c9d67ab09bdf007fdc2
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-04 14:19:32 -0300

    register multer middleware and add file route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Router from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,4 +18,5 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;

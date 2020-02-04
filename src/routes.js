f1fd5734a413e3963926bd82fced2a426c06829f
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-04 15:11:12 -0300

    list providers and static files at uploads folder

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Router from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
@@ -17,6 +18,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;

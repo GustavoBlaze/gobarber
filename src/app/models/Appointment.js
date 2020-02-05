2857cec78571c10e27a4d65ed16906de23ed38ca
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-05 13:39:51 -0300

    fix associate with user on appointment

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -15,8 +15,8 @@ class Appointment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.USER, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.USER, { foreignKey: 'provider_id', as: 'provider' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 

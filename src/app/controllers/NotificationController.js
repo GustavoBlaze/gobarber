7b3091c7a202ecf1dca78b68a1cddd75674fff57
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-07 13:47:13 -0300

    list notifications and check as read

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,41 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only providers can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    // const notification = await Notification.findById(req.params.id);
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();

d8b7d0e49e66047b6b3d51110d6e1d8a62732f42
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-02-07 13:45:51 -0300

    email configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd50b557b47e479',
+    pass: '505347bcd0149e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

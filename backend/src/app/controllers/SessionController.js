565602bfc0c9e0ebfdaedc898a7457f0faa0fbc5
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-15 13:30:06 -0300

    feat: bring avatar on new login

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import File from '../models/File';
 
 import authConfig from '../../config/auth';
 
@@ -19,7 +20,16 @@ class SessionController {
     }
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'user not found' });
@@ -29,13 +39,14 @@ class SessionController {
       return res.status(401).json({ error: 'password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,

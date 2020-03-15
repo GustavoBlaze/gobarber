151a95457ae6916c1936a086b5822835a3492955
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-15 16:06:00 -0300

    style: signin and signup

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>signup</h1>;
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+
+      <form>
+        <input type="text" placeholder="Home completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }

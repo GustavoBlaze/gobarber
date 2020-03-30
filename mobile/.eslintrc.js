b2513699317f0c341c584b464ee80a07aa27bec6
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-30 20:59:48 -0300

    chore: eslint enable spreading

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": "off"
   },
   settings: {
     "import/resolver": {

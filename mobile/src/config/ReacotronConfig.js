139bee6db8755e28921c39aa69b2f3291ded6b20
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-31 12:21:26 -0300

    feat: add redux and redux saga

--- mobile/src/config/ReacotronConfig.js
+++ mobile/src/config/ReacotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure()
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())

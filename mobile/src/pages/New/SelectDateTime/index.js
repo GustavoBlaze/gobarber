512a83bb857fd0fa8edd949abc4b867ef027d33e
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-01 12:11:23 -0300

    feat: date picker

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
+
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
 });

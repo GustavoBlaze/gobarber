002ec52d4139d04c5fd481635461ab1b33d70064
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-01 10:02:08 -0300

    feat: fetch and list appointments from api

--- mobile/src/components/Appointment/index.js
+++ mobile/src/components/Appointment/index.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ data, onCancel }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
-          source={{ uri: 'https://api.adorable.io/avatar/50/gustavo.png' }}
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
+          }}
         />
         <Info>
-          <Name>Luiz Gustavo</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
+
+Appointment.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    past: PropTypes.bool.isRequired,
+    provider: PropTypes.shape({
+      avatar: PropTypes.shape({
+        url: PropTypes.string.isRequired,
+      }).isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    cancelable: PropTypes.bool.isRequired,
+    canceled_at: PropTypes.string,
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
@@ -6,26 +7,59 @@ import Appointment from '~/components/Appointment';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
+import api from '~/services/api';
+
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+    setAppointments(
+      appointments.map((appointment) =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={(item) => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Background>
   );
 }
 
+const TabBarIcon = ({ tintColor }) => (
+  <Icon name="event" size={20} color={tintColor} />
+);
+
+TabBarIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
 Dashboard.navigationOptions = {
   tabBarLabel: 'Agendamentos',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
-  ),
+  tabBarIcon: TabBarIcon,
 };

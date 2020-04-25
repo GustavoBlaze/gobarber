51bf748a2bc7ebb1b4da4e68a879df388a87bb7a
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-04-25 20:44:20 -0300

    chore: change default database port

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 5433,
+  port: 5432,
   define: {
     timestamps: true,
     underscored: true,
